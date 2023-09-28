6949000e08bada7f903169eb5263756ec00a990a
EZhitnikovich <ezhitnikovich@gmail.com>
2023-09-28 19:22:19 +0300

    add models

---
+++ api/src/Models/Activity.cs
@@ -0,0 +1,11 @@
+﻿namespace TimeTrackerApi.Models
+{
+    public class Activity: BaseEntity
+    {
+        public string Description { get; set; } = string.Empty;
+        public List<Tag> Tags { get; set; }
+        public Project? Project { get; set; }
+        public int StartInMilliseconds { get; set; }
+        public int? EndInMilliseconds { get; set;}
+    }
+}
---
+++ api/src/Models/Project.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TimeTrackerApi.Models
+{
+    public class Project: BaseEntity
+    {
+        [Required]
+        [MinLength(1)]
+        public string Title { get; set; } = string.Empty;
+    }
+}
