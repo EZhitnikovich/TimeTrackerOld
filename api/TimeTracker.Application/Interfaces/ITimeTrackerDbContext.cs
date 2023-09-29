1a5f529321fe0c7932419b7b0fa912fc1eaa868f
EZhitnikovich <ezhitnikovich@gmail.com>
2023-09-29 18:59:07 +0300

    add ITimeTrackerDbContext.cs

---
+++ api/TimeTracker.Application/Interfaces/ITimeTrackerDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TimeTracker.Domain;
+
+namespace TimeTracker.Application.Interfaces
+{
+    public interface ITimeTrackerDbContext
+    {
+        DbSet<Activity> Activities { get; set; }
+        DbSet<Project> Projects { get; set; }
+        DbSet<Tag> Tags { get; set; }
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
