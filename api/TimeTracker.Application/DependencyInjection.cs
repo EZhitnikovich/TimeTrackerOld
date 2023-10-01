72c4b42f39dd49808b37026241c5d86a225d873a
EZhitnikovich <ezhitnikovich@gmail.com>
2023-10-01 22:59:45 +0300

    add DI

---
+++ api/TimeTracker.Application/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace TimeTracker.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly())); // TODO: mb smth wrong
+            return services;
+        }
+    }
+}
---
+++ api/TimeTracker.Persistence/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TimeTracker.Application.Interfaces;
+
+namespace TimeTracker.Persistence
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
+        {
+            var conectionString = configuration["DbConnection"];
+            services.AddDbContext<TimeTrackerDbContext>(options =>
+            {
+                options.UseSqlite(conectionString);
+            });
+            services.AddScoped<ITimeTrackerDbContext>(provider => provider.GetService<TimeTrackerDbContext>());
+            return services;
+        }
+    }
+}
